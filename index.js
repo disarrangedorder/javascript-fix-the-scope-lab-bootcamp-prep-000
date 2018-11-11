bd33b0bf558e593cfe4b201a52dbb724b6152044
Claire Nguyen <disarrangedorder@gmail.com>
2018-11-11 19:27:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
 
-
+//Error: Expected [Function: funkyFunction] to equal 'FUNKY!'
 //Questions:
 //I ran the following code and it returned "FUNKY!" in the console. Why does this not pass the test?
 //var theFunk = funkyFunction()
