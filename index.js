fb513cc71453748cd448855afa47edb30ced17c1
Claire Nguyen <disarrangedorder@gmail.com>
2018-11-11 19:15:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 funkyFunction()
+theFunk()
 console.log(theFunk())
 
 //Questions:
